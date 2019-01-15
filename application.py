b12243bfd187820f6b531e8eaba9a36aa092861b
Emma Tas <emma.tas@live.nl>
2019-01-15 10:08:14 +0000

    dit heb ik net gedaan

--- application.py
+++ application.py
@@ -18,8 +18,6 @@ if app.config["DEBUG"]:
         response.headers["Pragma"] = "no-cache"
         return response
 
-# custom filter
-app.jinja_env.filters["usd"] = usd
 
 # configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
@@ -27,72 +25,26 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# configure CS50 Library to use SQLite database
-db = SQL("sqlite:///instapet.db")
-
 
 @app.route("/")
 @login_required
 def index():
-    cash = db.execute("SELECT cash FROM users WHERE id = :userID", userID=session["user_id"])[0]['cash']
-    shares = db.execute("SELECT * FROM stock WHERE userid = :userID", userID=session["user_id"])
-    print(shares)
-    geldaandelen = 0
-    for x in range(len(shares)):
-        merk = shares[x]["symbool"]
-        quote = lookup(merk)["price"]
-        shares[x]["price"] = usd(quote)
-        totaal = quote * float(shares[x]["aantal"])
-        shares[x]["totaal"] = usd(totaal)
-        geldaandelen += totaal
-    samen = geldaandelen + cash
-    samen = usd(samen)
-    aandelen = usd(geldaandelen)
-    return render_template("index.html",
-                           cash=usd(cash),
-                           shares=shares,
-                           samen=samen,
-                           aandelen=aandelen)
+
+    return render_template("index.html")
 
 
-@app.route("/buy", methods=["GET", "POST"])
+@app.route("/profile", methods=["GET", "POST"])
 @login_required
-def buy():
-    """Buy shares of stock."""
-    if request.method == "POST":
-        symbool = request.form.get("symbol")
-        symbool = symbool.upper()
-        quote = lookup(symbool)
-        if quote == None:
-            return apology("Invalid symbol")
-        amount = request.form.get("shares")
-        try:
-            amount = (float(amount))
-        except:
-            return apology("Give a number!")
-        if amount < 1:
-            return apology("Must buy at least 1 share")
-        if amount % 1 != 0:
-            return apology("Buy whole shares!")
-        geld = db.execute("SELECT cash FROM users WHERE id = :userID", userID=session["user_id"])
-        print(geld)
-        prijs = (quote['price'] * float(amount))
-        if prijs > geld[0]['cash']:
-            return apology("Not enough money")
-        db.execute("UPDATE users SET cash = cash - :totaalprijs WHERE id = :userID", userID=session["user_id"], totaalprijs=prijs)
-        try:
-            oud = db.execute("SELECT aantal FROM stock WHERE userid = :userID AND symbool= :symbool",
-                             userID=session["user_id"], symbool=symbool)[0]['aantal']
-            db.execute("UPDATE stock SET aantal = :aantal WHERE userid = :userID AND symbool= :symbool",
-                       userID=session["user_id"], aantal=oud+int(amount), symbool=symbool)
-        except:
-            db.execute("Insert INTO stock (userid, symbool, aantal) VALUES(:userid, :symbool, :aantal)",
-                       userid=session["user_id"], symbool=symbool, aantal=int(amount))
-        db.execute("Insert INTO history (userid, symbol, amount, price, total) VALUES(:userid, :symbol, :amount, :price, :total)",
-                   userid=session["user_id"], symbol=symbool, amount=amount, price=quote['price'], total=prijs)
-        return render_template("buy.html", name=str(symbool) + ' was bought for: ' + str(usd(prijs)),
-                               cash='Balance left: ' + usd(db.execute("SELECT cash FROM users WHERE id = :userID", userID=session["user_id"])[0]['cash']))
-    return render_template("buy.html")
+def profile():
+    # Laad profielfoto
+    #Laad bio
+    #Laad aantal volgers
+    #Laad foto's geplaatst door profiel
+    #Laad gelikte foto's door profiel
+    #Laad likes en dislikes op
+    #Controle of gebruiker eigenaar is van profiel
+    #Zo ja, laad bewerkknop voor profiel die redirect naar profiel beheerpagina.
+    return render_template("profile.html")
 
 
 @app.route("/history")
