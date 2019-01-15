be7647650015ade55903f9bddad2ab7d6ce417f1
Emma Tas <emma.tas@live.nl>
2019-01-15 10:22:28 +0000

    Er staat wat we moeten doen in application.py

--- application.py
+++ application.py
@@ -47,47 +47,36 @@ def profile():
     return render_template("profile.html")
 
 
-@app.route("/history")
-@login_required
-def history():
-    """Show history of transactions."""
-    geschiedenis = db.execute("SELECT * FROM history WHERE userid = :userID", userID=session["user_id"])
-    for x in range(len(geschiedenis)):
-        geschiedenis[x]['price'] = usd(geschiedenis[x]['price'])
-        geschiedenis[x]['total'] = usd(geschiedenis[x]['total'])
-    print(geschiedenis)
-    return render_template("history.html", history=geschiedenis)
+
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     """Log user in."""
+
+#     # forget any user_id
+#     session.clear()
+
+#     # if user reached route via POST (as by submitting a form via POST)
+#     if request.method == "POST":
+
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+
+#         # ensure username was submitted
+#         if not username:
+#             return apology("must provide username")
+
+#         # ensure password was submitted
+#         elif not password:
+#             return apology("must provide password")
+
+#         login(username, password)
+
+#     # else if user reached route via GET (as by clicking a link or via redirect)
+#     else:
+#         return render_template("index.html")
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    """Log user in."""
-
-    # forget any user_id
-    session.clear()
-
-    # if user reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        # ensure username was submitted
-        if not username:
-            return apology("must provide username")
-
-        # ensure password was submitted
-        elif not password:
-            return apology("must provide password")
-
-        login(username, password)
-
-    # else if user reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
-
-
-@app.route("/logout")
+# @app.route("/logout")
 def logout():
     """Log user out."""
 
@@ -95,137 +84,78 @@ def logout():
     session.clear()
 
     # redirect user to login form
-    return redirect(url_for("login"))
+    return redirect(url_for("index"))
 
 
-@app.route("/quote", methods=["GET", "POST"])
+@app.route("/beheer", methods=["GET", "POST"])
 @login_required
-def quote():
-    """Get stock quote."""
-    if request.method == "POST":
-        quote = lookup(request.form.get("symbol"))
-        if quote == None:
-            return apology("Invalid symbol")
-        return render_template("quote.html", name='Look at this awesome Quote!',
-                               Name='Brand: ' + str(quote['name']),
-                               Name2='Price: ' + usd(quote['price']))
-    return render_template("quote.html")
+def beheer():
+    # Controle voor sessie gebruiker (Gebruiker moet ingelogt zijn)
+    # Laad velden met aanpasbare gegevens
+    # Vul velden met huidige data uit database. (Kan ook default waarde zijn.) Na drukken op 'bijwerken'
+    # Controle voor correctheid (Velden niet leeg?, maximaal aantal karakters niet overschreden)
+    # Aangepaste gegevens in database updaten
+    # Doorverwijzen naar profielpagina
+    return render_template("beheer.html")
 
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    """Register user."""
+# @app.route("/register", methods=["GET", "POST"])
+# def register():
+#     """Register user."""
 
-    # forget any user_id
-    session.clear()
+#     # forget any user_id
+#     session.clear()
 
-    if request.method == "POST":
+#     if request.method == "POST":
 
-        # ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username")
+#         # ensure username was submitted
+#         if not request.form.get("username"):
+#             return apology("must provide username")
 
-        # ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password")
+#         # ensure password was submitted
+#         elif not request.form.get("password"):
+#             return apology("must provide password")
 
-        # ensure password was submitted
-        elif not request.form.get("confirmation"):
-            return apology("must provide both passwords")
+#         # ensure password was submitted
+#         elif not request.form.get("confirmation"):
+#             return apology("must provide both passwords")
 
-        # ensure passwords match
-        elif request.form.get("confirmation") != request.form.get("password"):
-            return apology("must fill in same password")
+#         # ensure passwords match
+#         elif request.form.get("confirmation") != request.form.get("password"):
+#             return apology("must fill in same password")
 
-        hash = pwd_context.hash(request.form.get("password"))
-        variable = db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)",
-                              username=request.form.get("username"), hash=hash)
+#         hash = pwd_context.hash(request.form.get("password"))
+#         variable = db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)",
+#                               username=request.form.get("username"), hash=hash)
 
-        if not variable:
-            return apology("Username already present")
+#         if not variable:
+#             return apology("Username already present")
 
-        session["user_id"] = variable
+#         session["user_id"] = variable
 
-        return redirect(url_for("index"))
-    return render_template("register.html")
+#         return redirect(url_for("index"))
+#     return render_template("index.html")
 
 
-@app.route("/balance", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
 @login_required
-def balance():
-    """Gives user money"""
-    if request.method == "POST":
-        hoeveelheid = request.form.get("hoeveelheid")
-        try:
-            hoeveelheid = float(hoeveelheid)
-        except:
-            return apology("must be a number")
-        if not hoeveelheid >= 1:
-            return apology("Must be at least 1")
-        if hoeveelheid % 1 != 0:
-            return apology("Must be whole dollars!")
-
-        db.execute("UPDATE users SET cash = cash + :hoeveelheid WHERE id = :userID",
-                   userID=session["user_id"], hoeveelheid=hoeveelheid)
-        return render_template("balance.html", name=usd(hoeveelheid) + " was added to your account!",
-                               name2="Your current balance is: " + usd(db.execute("SELECT cash FROM users WHERE id = :userID",
-                                                                                  userID=session["user_id"])[0]['cash']))
-        return apology("nog niet klaar")
-    return render_template("balance.html")
+def home():
+    # Controle voor sessie gebruiker (Gebruiker moet ingelogt zijn)
+    # Laad random fotoID
+    # Controle of gebruiker bericht al eens heeft beoordeeld (Loop door random fotoID's tot foto wordt gevonden, die nog niet is beoordeeld door gebruiker.)
+    # Laad foto met bijbehorende caption, comments, profielfoto van plaatser, profielnaam van plaatser, titel, timestamp.
+    # Laad like en dislike knop
+    # Laad share mogelijkheden
+    return render_template("home.html")
 
 
-@app.route("/sell", methods=["GET", "POST"])
+@app.route("/pack", methods=["GET", "POST"])
 @login_required
-def sell():
-    """Sell shares of stock."""
-    if request.method == "POST":
-        symbool = request.form.get("symbol")
-        quote = lookup(symbool)
-        if quote == None:
-            return apology("Invalid symbol")
-        aantal = request.form.get("shares")
-        try:
-            aantal = (float(aantal))
-        except:
-            return apology("Give a number!")
-        if aantal < 1:
-            return apology("Sell at least 1 share")
-        if aantal % 1 != 0:
-            return apology("Sell whole shares!")
-        aantal = int(aantal)
-        prijs = (quote['price'] * float(aantal))
-        try:
-            oud = db.execute("SELECT aantal FROM stock WHERE userid = :userID AND symbool= :symbool",
-                             userID=session["user_id"], symbool=symbool)[0]['aantal']
-            print(oud)
-            print(aantal)
-            if oud < aantal:
-                return apology("You cant sell this much!")
-            if oud == aantal:
-                db.execute("DELETE FROM stock WHERE userid = :userID AND symbool= :symbool",
-                           userID=session["user_id"], symbool=symbool)
-            else:
-                db.execute("UPDATE stock SET aantal = :aantal WHERE userid = :userID AND symbool= :symbool",
-                           userID=session["user_id"], aantal=(oud-aantal), symbool=symbool)
-            db.execute("UPDATE users SET cash = cash + :totaalprijs WHERE id = :userID",
-                       userID=session["user_id"], totaalprijs=prijs)
-            db.execute("Insert INTO history (userid, symbol, amount, price, total) VALUES(:userid, :symbol, :amount, :price, :total)",
-                       userid=session["user_id"], symbol=symbool, amount=-aantal, price=quote['price'], total=prijs)
-        except:
-            return apology("You cant sell if you dont own this stock!")
-        merken = []
-        oud = db.execute("SELECT symbool FROM stock WHERE userid = :userID AND aantal > 0", userID=session["user_id"])
-        for x in range(len(oud)):
-            merken.append(oud[x]['symbool'])
-        return render_template("sell.html", name='Brand: ' + str(request.form.get('symbol')),
-                               prijs='Price: ' + str(usd(quote['price'])),
-                               aantal='Amount: ' + str(int(aantal)),
-                               winst='Profit: ' + str(usd(prijs)),
-                               geld='Balance left: ' + str(usd(db.execute("SELECT cash FROM users WHERE id = :userID",
-                                                                          userID=session["user_id"])[0]['cash'])),
-                               symbolen=merken)
-    merken = []
-    oud = db.execute("SELECT symbool FROM stock WHERE userid = :userID AND aantal > 0", userID=session["user_id"])
-    for x in range(len(oud)):
-        merken.append(oud[x]['symbool'])
-    return render_template("sell.html", symbolen=merken)
+def pack():
+    # Controle voor sessie gebruiker (Gebruiker moet ingelogt zijn)
+    # Laad fotoID van profiel uit database met gevolgden door gebruiker (chronologische volgorde)
+    # Controle of gebruiker bericht al eens heeft beoordeeld (Loop door random fotoID's tot foto wordt gevonden, die nog niet is beoordeeld door gebruiker.)
+    # Laad foto met bijbehorende caption, comments, profielfoto van plaatser, profielnaam van plaatser, titel, timestamp.
+    # Laad like en dislike knop
+    # Laad share mogelijkheden
+    return render_template("pack.html)
